65141f83a5d4fd37cac9ef7de3947bb1e62fbbb7
DESKTOP\David <davidalejangp@gmail.com>
2026-05-11 17:16:16 -0500

    ahora se hace migracion siempre

---
+++ instrumentation.ts
@@ -0,0 +1,14 @@
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    const { migrate } = await import("drizzle-orm/node-postgres/migrator");
+    const { Pool } = await import("pg");
+    const { drizzle } = await import("drizzle-orm/node-postgres");
+    const path = await import("path");
+
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const db = drizzle(pool);
+
+    await migrate(db, { migrationsFolder: path.join(process.cwd(), "drizzle") });
+    await pool.end();
+  }
+}
